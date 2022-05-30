5b2833d4947f58e8646f7a97659ef538945311be
anlyon <alyon@phys.ethz.ch>
2022-05-30 10:49:52 +0200

    submitter modified

--- TagAndProbe/test/submitter.sh
+++ TagAndProbe/test/submitter.sh
@@ -11,17 +11,23 @@ echo $workdir
 
 mkdir -p $workdir
 cp tagProbeFitTreeAnalyzer_JPsiMuMu_cfg.py $workdir
+cp ${1} $workdir/file.txt
 cd $workdir
 
 echo "running script"
-cmsRun tagProbeFitTreeAnalyzer_JPsiMuMu_cfg.py infilename=${1} outLabel=${2} outSuffix=${3} 
+DATE_START=`date +%s`
+cmsRun tagProbeFitTreeAnalyzer_JPsiMuMu_cfg.py inputFiles_load=file.txt outLabel=${2} outSuffix=${3} 
+DATE_END=`date +%s`
 
 echo "coyping the files"
 #cp -r results_${2}_${3}.root $homedir/outfiles/${2}
-cp -r results_${2}_${3}.root /work/anlyon/tag_and_probe/outfiles/${2}
+mv results_${2}_${3}.root /work/anlyon/tag_and_probe/outfiles/${2}/
 
 cd $homedir
 rm -r $workdir
+
+runtime=$((DATE_END-DATE_START))
+echo "Wallclock running time: $runtime s"
 
 echo "End"
 
